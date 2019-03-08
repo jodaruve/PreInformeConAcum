13ce1f289812f4ed1b033e3db9c80b46550e5fb3
Rueda Velasco <000400436@bga.upb>
2019-03-08 09:41:27 -0500

    problema resuelto

--- mayor numero.py
+++ mayor numero.py
@@ -1,11 +1,10 @@
 ## mayor numero ingresado
 num1=0
 i=0
-num=int(input("ingrese un numero"))
 while i < 6:
+  num=int(input("ingrese un numero"))
   i=i+1
   num1=num
   if num > num1:
     num1 = num
-  num=int(input("ingrese un numero"))
 print(num1)
